58bacb68c21fb8034d1bdd21ccdc5eb30a25c3cc
Alexander Voigt <ahgvoigt@gmail.com>
2021-02-01 10:35:40 +0100

    initial commit
    Collection of Passarino-Veltman 1-loop functions

---
+++ src/degrassi.c
@@ -0,0 +1,83 @@
+#include <math.h>
+
+
+/*
+ * Degrassi and Sirlin, Phys. Rev. D46 (1992) 3104,
+ * Appendix A, Eq.(A3)
+ */
+static double omega(double a, double b)
+{
+   const double C = 0.5 * (a + b) - 0.25 * (a - b)*(a - b) - 0.25;
+   double res = 0;
+
+   if (C > 0) {
+      const double sC = sqrt(C);
+      res = sC * (atan((1 + a - b) / (2 * sC)) +
+                  atan((1 - a + b) / (2 * sC)));
+   } else if (C < 0) {
+      const double sC = sqrt(-C);
+      res = 0.5 * sC *
+            log((0.5 * (a + b - 1) - sC) /
+                (0.5 * (a + b - 1) + sC));
+   } else {
+      res = 0;
+   }
+
+   return res;
+}
+
+
+/*
+ * Real part of B0 1-loop function.
+ *
+ * From Degrassi and Sirlin, Phys. Rev. D46 (1992) 3104,
+ * Appendix A, Eq.(A2)
+ *
+ * @param p2 squared momentum
+ * @param m1 squared mass
+ * @param m2 squared mass
+ * @param q2 squared renormalization scale
+ * @return real part of B0
+ */
+double b0_degrassi(double p2, double m1, double m2, double q2)
+{
+   double res = 0;
+
+   if (p2 == 0) {
+      if (m1 == 0 && m2 != 0) {
+         res = 1 - log(m2 / q2);
+      } else if (m1 != 0 && m2 == 0) {
+         res = 1 - log(m1 / q2);
+      } else if (m1 == m2) {
+         res = -log(m1 / q2);
+      } else {
+         res = 1 - log(m2 / q2) + m1 / (m1 - m2) * log(m2 / m1);
+      }
+   } else {
+      if (m1 == 0 && m2 != 0) {
+         if (m2 != p2) {
+            res = -(log(m2 / q2) - 2 -
+                    (m2 / p2 - 1) * log(fabs(1 - p2 / m2)));
+         } else {
+            res = -(log(m2 / q2) - 2);
+         }
+      } else if (m2 == 0 && m1 != 0) {
+         if (m1 != p2) {
+            res = -(log(m1 / q2) - 2 -
+                    (m1 / p2 - 1) * log(fabs(1 - p2 / m1)));
+         } else {
+            res = -(log(m1 / q2) - 2);
+         }
+      } else if (m2 == 0 && m1 == 0) {
+         res = -(log(p2 / q2) - 2);
+      } else {
+         const double delta = (m1 - m2)/p2;
+         res = -(log(p2 / q2) - 2 +
+                 0.5 * (1 + delta) * log(m1 / p2) +
+                 0.5 * (1 - delta) * log(m2 / p2) +
+                 2 * omega(m1 / p2, m2 / p2));
+      }
+   }
+
+   return res;
+}
---
+++ src/degrassi.h
@@ -0,0 +1,4 @@
+#pragma once
+
+/* real part of B0, from Degrassi and Sirlin, Phys. Rev. D46 (1992) 3104 */
+double b0_degrassi(double p2, double m1, double m2, double q2);
