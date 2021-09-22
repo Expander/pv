b7e79ff166aa42d09b45e9111dd95de17904147c
Alexander Voigt <ahgvoigt@gmail.com>
2021-09-22 17:31:34 +0200

    adding test stub

--- src/pv.h
+++ src/pv.h
@@ -1,4 +1,12 @@
 #pragma once
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* real part of B0, from Degrassi and Sirlin, Phys. Rev. D46 (1992) 3104 */
 double b0_degrassi(double p2, double m1, double m2, double q2);
+
+#ifdef __cplusplus
+}
+#endif
--- test/test_b.cpp
+++ test/test_b.cpp
@@ -1,4 +1,29 @@
+#include "pv.h"
+#include <cstdio>
+
+unsigned failed = 0;
+unsigned passed = 0;
+
+void check(bool cond)
+{
+   if (cond) {
+      passed++;
+   } else {
+      std::printf("test failed\n");
+      failed++;
+   }
+}
+
+void test_b0()
+{
+   check(b0_degrassi(0,1,1,1) == 0.0);
+}
+
 int main()
 {
-   return 0;
+   test_b0();
+
+   std::printf("Passed tests: [%u/%u]\n", passed, passed + failed);
+
+   return failed != 0;
 }
