c4411a3297fe35f55bb12d6051d20b5cb397e1ca
Alexander Voigt <ahgvoigt@gmail.com>
2021-09-24 06:52:16 +0200

    adding stub for benchmark

---
+++ test/bench.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "stopwatch.hpp"
+#include <algorithm>
+#include <complex>
+#include <random>
+#include <vector>
+
+namespace pv {
+namespace bench {
+
+template <class T>
+std::vector<T> generate_random_scalars(int n, T start, T stop)
+{
+   static std::minstd_rand gen;
+   std::uniform_real_distribution<T> dist(start, stop);
+
+   std::vector<T> v(n);
+   std::generate(begin(v), end(v),
+                 [&dist](){ return dist(gen); });
+
+   return v;
+}
+
+template <class T>
+std::vector<std::complex<T>> generate_random_complexes(
+   int n, T start, T stop)
+{
+   const auto reals = generate_random_scalars<T>(n, start, stop);
+   const auto imags = generate_random_scalars<T>(n, start, stop);
+
+   std::vector<std::complex<T>> v(n);
+
+   for (int i = 0; i < n; i++) {
+      v[i] = std::complex<T>(reals[i], imags[i]);
+   }
+
+   return v;
+}
+
+template <class T>
+inline void do_not_optimize(const T& value)
+{
+   asm volatile("" : : "r,m"(value) : "memory");
+}
+
+template <class F>
+double time_in_seconds(F&& f)
+{
+   polylogarithm::Stopwatch sw;
+   sw.start();
+   f();
+   sw.stop();
+   return sw.get_time_in_seconds();
+}
+
+} // namespace bench
+} // namespace pv
---
+++ test/bench_b.cpp
@@ -0,0 +1,85 @@
+#include "bench.hpp"
+#include "pv.hpp"
+#include <array>
+#include <string>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+template <typename T, typename Fn>
+void bench_fn(Fn f, const std::vector<T>& values, const std::string& name,
+              const std::string& type)
+{
+   // warm-up
+   for (const auto& v: values) {
+      pv::bench::do_not_optimize(f(v[0], v[1], v[2], v[3]));
+   }
+
+   const auto total_time = pv::bench::time_in_seconds([&] {
+         for (const auto& v: values) {
+            pv::bench::do_not_optimize(f(v[0], v[1], v[2], v[3]));
+         }
+      });
+
+   std::cout << std::setw(24) << std::left << name << "type: " << std::setw(16)
+             << std::left << type << "time: " << total_time << "s\n";
+}
+
+void print_line(char c)
+{
+   for (int i = 0; i < 60; ++i) {
+      std::cout << c;
+   }
+   std::cout << '\n';
+}
+
+void print_headline(const std::string& text)
+{
+   print_line('=');
+   std::cout << text << '\n';
+   print_line('=');
+}
+
+template<typename T>
+void bench(const T& values_d)
+{
+   print_headline("B0");
+
+   bench_fn([&](double s, double x, double y, double q) {
+               return b0_degrassi(s,x,y,q);
+            },
+            values_d, "Degrassi", "real");
+
+   bench_fn([&](double s, double x, double y, double q) {
+               return b0_softsusy(s,x,y,q);
+            },
+            values_d, "SOFTSUSY", "real");
+}
+
+int main()
+{
+   using pv::bench::generate_random_scalars;
+   using pv::bench::generate_random_complexes;
+
+   const std::size_t N = 1000000;
+   const auto min = 0;
+   const auto max = 4;
+
+   const auto s = generate_random_scalars<double>(N, min, max);
+   const auto x = generate_random_scalars<double>(N, min, max);
+   const auto y = generate_random_scalars<double>(N, min, max);
+   const auto q = generate_random_scalars<double>(N, min, max);
+
+   std::vector<std::array<double, 4>> values(N);
+
+   for (std::size_t i = 0; i < N; ++i) {
+      values[i][0] = s[i];
+      values[i][1] = x[i];
+      values[i][2] = y[i];
+      values[i][3] = q[i];
+   }
+
+   bench(values);
+
+   return 0;
+}
