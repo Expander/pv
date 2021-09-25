66feb92d9306f341cecd5e581b0bac0a703b2c13
Alexander Voigt <ahgvoigt@gmail.com>
2021-09-25 12:01:47 +0200

    adding precision function for x = y case

--- src/b0.cpp
+++ src/b0.cpp
@@ -27,6 +27,56 @@ double omega(double a, double b) noexcept
    return res;
 }
 
+/**
+ * Re(B0(s,x,x,q)), Eq.(2.4) from [hep-ph/0701051]
+ *
+ * @param s squared momentum
+ * @param x squared mass
+ * @param q squared renormalization scale
+ *
+ * @return Re(B0(s,x,x,q))
+ */
+double b0xx(double s, double x, double q) noexcept
+{
+   constexpr double EPSTOL = 1.0e-15; ///< underflow accuracy
+
+   if (s < EPSTOL * q && x < EPSTOL * q) {
+      return 0; // IR divergence
+   } else if (s < 1e-3*x) {
+      const double d = s / x;
+      return -std::log(x / q)
+         + d*(1./6
+         + d*(1./60
+         + d*(1./420
+         + d*(1./2520
+         + d*(1./13860
+         + d*(1./72072
+         + d/360360))))));
+   } else if (s <= 4 * x) {
+      return 2 - std::log(x / q) -
+             2 * std::sqrt(4 * x / s - 1) *
+                std::asin(std::sqrt(s / (4 * x)));
+   } else if (s < 1e2 * x) {
+      const double sq = std::sqrt(1 - 4 * x / s);
+      return 2 - std::log(x / q) +
+         sq * std::log(s * (1 - sq) / (2 * x) - 1);
+   } else if (s*EPSTOL < x) {
+      const double d = x / s;
+      const double logd = std::log(d);
+      return 2 - std::log(s / q)
+         + d * (2 * (1 - logd)
+         + d * (-1 - 2 * logd
+         + d * (-10./3 - 4 * logd
+         + d * (-59./6 - 10 * logd
+         + d * (-449./15 - 28 * logd
+         + d * (-1417./15 - 84 * logd
+         + d * (-32254./105 - 264 * logd
+         + d * (-429697./420 - 858 * logd))))))));
+   } else {
+      return 2 - std::log(s / q);
+   }
+}
+
 } // anonymous namespace
 
 namespace pv {
@@ -72,15 +122,7 @@ double b0(double s, double x, double y, double q) noexcept
       } else if (y == 0 && x == 0) {
          res = -std::log(s / q) + 2;
       } else if (x == y) {
-         if (s <= 4 * y) {
-            res = -std::log(y / q) + 2
-                  - 2 * std::sqrt(4 * y / s - 1)
-                      * std::asin(std::sqrt(s / (4 * y)));
-         } else {
-            const double sC = std::sqrt(1 - 4 * y / s);
-            res = -std::log(y / q) + 2
-                  + sC * std::log(s * (1 - sC) / (2 * y) - 1);
-         }
+         res = b0xx(s, x, q);
       } else {
          const double a = x / s;
          const double b = y / s;
