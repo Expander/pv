8d95095808838479c4d61fc59a0086d3746eada4
Alexander Voigt <ahgvoigt@gmail.com>
2022-12-05 08:40:34 +0100

    adding alternative implementation of B0

--- math/pv.m
+++ math/pv.m
@@ -24,3 +24,30 @@ B0[p_, m1_, m2_, q_] :=
            Series[Delta - Log[p/q] - fB[xp] - fB[xm],
                   {eps, 0, 0}] // Normal // Refine
           ];
+
+(* ****** alternative implementation of B0 ****** *)
+
+(* Degrassi and Sirlin, Phys. Rev. D46 (1992) 3104 *)
+(* Arguments are interpreted as squared. *)
+B02[p_, m1_, m2_, q_] :=
+    Module[{a = x / s, b = y / s, delta = a - b},
+           (Delta - Log[s / q] + 2
+            - (1 + delta)/2 Log[a]
+            - (1 - delta)/2 Log[b]
+            - 2 Omega[a, b])
+    ];
+
+Omega[a_, b_] :=
+    Omega[a, b, (a + b)/2 - (a - b)(a - b)/4 - 1/4];
+
+Omega[a_, b_, C_] :=
+    Module[{sC = Sqrt[C]},
+           sC (ArcTan[(1 + a - b) / (2 sC)] + ArcTan[(1 - a + b) / (2 sC)])
+    ] /; C > 0;
+
+Omega[a_, b_, C_] :=
+    Module[{sC = Sqrt[-C]},
+           sC / 2 Log[((a + b - 1)/2 - sC) / ((a + b - 1)/2 + sC)]
+    ] /; C < 0;
+
+Omega[a_, b_, C_] := 0 /; PossibleZeroQ[C];
