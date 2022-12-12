249ce2b51047eaf5fa2a3eea30dae8b055bca45f
Alexander Voigt <ahgvoigt@gmail.com>
2022-12-12 19:50:07 +0100

    call a0 function

--- src/run.cpp
+++ src/run.cpp
@@ -4,14 +4,22 @@
 
 int main(int argc, char* argv[])
 {
-   if (argc == 4) {
+   if (argc == 2) {
+      const double x = std::stod(argv[1]);
+      const double q = 1;
+      std::printf("Re[a0(x = %.17g, q = %.17g)] = %.17g\n", x, q, pv::a0(x, q));
+   } else if (argc == 4) {
       const double s = std::stod(argv[1]);
       const double x = std::stod(argv[2]);
       const double y = std::stod(argv[3]);
       const double q = 1;
       std::printf("Re[b0(s = %.17g, x = %.17g, y = %.17g, q = %.17g)] = %.17g\n", s, x, y, q, pv::b0(s, x, y, q));
    } else {
-      std::printf("Usage: <program-name> s x y (q = 1)\n");
+      std::printf(
+         "Usage: (q = 1\n"
+         "  <program-name> x\n"
+         "  <program-name> s x y\n"
+         );
    }
 
    return 0;
