3a9cf6f5f99c91dba3e728cd1388d8a2e72b33ac
Alexander Voigt <alexander.voigt@hs-flensburg.com>
2022-12-13 08:22:33 +0100

    return nan if q <= 0

--- src/a0.cpp
+++ src/a0.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <limits>
 
 namespace pv {
 
@@ -6,18 +7,19 @@ namespace pv {
  * Real part of A0 1-loop function.
  *
  * @param x squared mass
- * @param q squared renormalization scale
+ * @param q squared renormalization scale, q > 0
  * @return real part of A0
  */
 double a0(double x, double q) noexcept
 {
-   if (q == 0) {
-      return 0;
+   if (q <= 0) {
+      return std::numeric_limits<double>::quiet_NaN();
    }
 
    if (x == 0) {
       return 0;
    }
+
    return x * (1 - std::log(x/q));
 }
 
