3cf8c23310130b3157d1002b59af6e80cc335e3a
Alexander Voigt <alexander.voigt@hs-flensburg.com>
2022-12-13 12:40:04 +0100

    consistent naming of parameters

--- src/run.cpp
+++ src/run.cpp
@@ -24,7 +24,7 @@ int main(int argc, char* argv[])
       // A0 function
       const double x = string_to_double(argv[1]);
       const double q = string_to_double(argv[2]);
-      std::printf("Re[a0(x = %.17g, q = %.17g)] = %.17g\n",
+      std::printf("Re[a0(m^2 = %.17g, Q^2 = %.17g)] = %.17g\n",
                   x, q, pv::rea0(x, q));
    } else if (argc == 5) {
       // B0 function
@@ -32,7 +32,7 @@ int main(int argc, char* argv[])
       const double x = string_to_double(argv[2]);
       const double y = string_to_double(argv[3]);
       const double q = string_to_double(argv[4]);
-      std::printf("Re[b0(s = %.17g, x = %.17g, y = %.17g, q = %.17g)] = %.17g\n",
+      std::printf("Re[b0(p^2 = %.17g, m1^2 = %.17g, m2^2 = %.17g, Q^2 = %.17g)] = %.17g\n",
                   s, x, y, q, pv::reb0(s, x, y, q));
    } else {
       std::printf(
@@ -41,8 +41,8 @@ int main(int argc, char* argv[])
          "This program calculates n-point 1-loop integrals.\n"
          "The value of n depends on the number of given parameters:\n"
          "\n"
-         "  n = 1: <program-name> m^2 q^2\n"
-         "  n = 2: <program-name> p^2 m1^2 m2^2 q^2\n"
+         "  n = 1: <program-name> m^2 Q^2\n"
+         "  n = 2: <program-name> p^2 m1^2 m2^2 Q^2\n"
          "\n"
          "The parameters are always interpreted as squared masses or squared energies.\n"
          );
