c746047fd8b49255082cd0591a1f94d5bd65ad5b
Alexander Voigt <alexander.voigt@hs-flensburg.com>
2022-12-13 08:47:48 +0100

    adding test for A0 function

---
+++ test/test_a.cpp
@@ -0,0 +1,47 @@
+#include "pv.hpp"
+#include "read_data.hpp"
+#include "test.hpp"
+#include <cmath>
+#include <cstdio>
+
+
+template <typename Fn>
+void test_a0(Fn fn, double eps)
+{
+   const std::string filename =
+      std::string(TEST_DATA_DIR) + PATH_SEPARATOR + "A0.txt";
+   const auto data = read_from_file(filename);
+
+   for (const auto& v: data) {
+      if (v.size() < 6) {
+         continue;
+      }
+
+      const double m2 = v.at(0);
+      const double q2 = v.at(1);
+      const double re = v.at(2);
+
+      CHECK_CLOSE(fn(m2, q2), re, eps);
+   }
+}
+
+
+void test_a0()
+{
+   const auto a0 = [] (double x, double q) {
+      return pv::rea0(x, q);
+   };
+
+   test_a0(a0, 3e-11);
+}
+
+
+int main()
+{
+   test_a0();
+
+   std::printf("Failed tests: [%u/%u]\n", failed, passed + failed);
+   std::printf("Passed tests: [%u/%u]\n", passed, passed + failed);
+
+   return failed != 0;
+}
--- test/test_b.cpp
+++ test/test_b.cpp
@@ -1,35 +1,9 @@
 #include "alt.hpp"
 #include "pv.hpp"
 #include "read_data.hpp"
+#include "test.hpp"
 #include <cmath>
 #include <cstdio>
-
-unsigned failed = 0;
-unsigned passed = 0;
-
-
-#define CHECK_EQUAL(a, b)                                       \
-   do {                                                         \
-      if ((a) == (b)) {                                         \
-         passed++;                                              \
-      } else {                                                  \
-         std::printf("Error: expressions are not equal in line %d: %.17g != %.17g\n", __LINE__, (a), (b));     \
-         failed++;                                              \
-      }                                                         \
-   } while (false);
-
-
-#define CHECK_CLOSE(a, b, eps)                                  \
-   do {                                                         \
-      const double diff = std::abs((a) - (b));                  \
-      const double mmax = std::max(std::abs(a), std::abs(b));   \
-      if (diff <= eps*mmax) {                                   \
-         passed++;                                              \
-      } else {                                                  \
-         std::printf("Error: expressions are not close in line %d: %.17g != %.17g (relative deviation: %g)\n", __LINE__, (a), (b), diff/mmax); \
-         failed++;                                              \
-      }                                                         \
-   } while (false);
 
 
 template <typename Fn>
