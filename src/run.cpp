8740504c2715f41282fb89930c555506c2586ae5
Alexander Voigt <alexander.voigt@hs-flensburg.com>
2022-12-13 12:28:14 +0100

    allow for q as input

--- src/run.cpp
+++ src/run.cpp
@@ -20,18 +20,18 @@ double string_to_double(char* str)
 
 int main(int argc, char* argv[])
 {
-   if (argc == 2) {
+   if (argc == 3) {
       // A0 function
       const double x = string_to_double(argv[1]);
-      const double q = 1;
+      const double q = string_to_double(argv[2]);
       std::printf("Re[a0(x = %.17g, q = %.17g)] = %.17g\n",
                   x, q, pv::rea0(x, q));
-   } else if (argc == 4) {
+   } else if (argc == 5) {
       // B0 function
       const double s = string_to_double(argv[1]);
       const double x = string_to_double(argv[2]);
       const double y = string_to_double(argv[3]);
-      const double q = 1;
+      const double q = string_to_double(argv[4]);
       std::printf("Re[b0(s = %.17g, x = %.17g, y = %.17g, q = %.17g)] = %.17g\n",
                   s, x, y, q, pv::reb0(s, x, y, q));
    } else {
@@ -41,11 +41,10 @@ int main(int argc, char* argv[])
          "This program calculates n-point 1-loop integrals.\n"
          "The value of n depends on the number of given parameters:\n"
          "\n"
-         "  n = 1: <program-name> m^2\n"
-         "  n = 2: <program-name> p^2 m1^2 m2^2\n"
+         "  n = 1: <program-name> m^2 q^2\n"
+         "  n = 2: <program-name> p^2 m1^2 m2^2 q^2\n"
          "\n"
          "The parameters are always interpreted as squared masses or squared energies.\n"
-         "The renormalization scale Q is always set to Q = 1.\n"
          );
       return EXIT_FAILURE;
    }
